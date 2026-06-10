666c8afe5f727c9ce4ed3792a9df9510fbad6c5e
Diluksha <dilukshmalan@gmail.com>
2026-06-10 16:49:56 +0530

    Initial set up

---
+++ gameone/gameone/gameoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameoneApp.swift
+//  gameone
+//
+//  Created by Student2 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct gameoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
